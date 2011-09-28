ec34ee552be78f31e8c696b738fc2c593d650fb2
Grant Mathews <grant@causes.com>
2011-09-27 17:41:13 -0700

    Add multi-file read and stdin read.
    Sadly, this involved nearly doubling my line count. Sad days indeed.

--- lab1.hs
+++ lab1.hs
@@ -1,14 +1,21 @@
 import qualified Data.Map as Map
 import Data.Char
 import Data.List
+import System.Environment
 
 toLowerWords str = map (map toLower) (words str)
 sortPairs pairs = sortBy (\pairA pairB -> compare (snd pairA) (snd pairB)) pairs
-
 showHistogram pairs = intercalate "\n" (map (\pair -> (take (snd pair) (repeat 'X')) ++ " : " ++ (fst pair)) pairs)
 
 -- multiple file readin
 main = do
-  contents <- fmap toLowerWords $ readFile "test"
+  args <- getArgs
+
+  contents <- 
+    fmap (toLowerWords) $ 
+    if null args
+      then getContents
+      else fmap (intercalate "") $ sequence $ map readFile args
+
   let histogramData = Map.toList $ Map.fromListWith (+) (zip contents (repeat 1))
   putStrLn $ showHistogram $ sortPairs histogramData
