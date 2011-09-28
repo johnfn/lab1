ef4e16b305f1d0afa7884287f5a57f4c906a93da
Grant Mathews <grant@causes.com>
2011-09-27 19:14:41 -0700

    Hlint gave me some helpful tips.
    hlint is a really powerful tool! Gotta look into how it works later...

--- lab1.hs
+++ lab1.hs
@@ -1,21 +1,22 @@
 import qualified Data.Map as Map
 import Data.Char
 import Data.List
+import Data.Ord
 import System.Environment
 
 toLowerWords str = map (map toLower) (words str)
-sortPairs pairs = sortBy (\pairA pairB -> compare (snd pairA) (snd pairB)) pairs
-showHistogram pairs = intercalate "\n" (map (\pair -> (take (snd pair) (repeat 'X')) ++ " : " ++ (fst pair)) pairs)
+sortPairs = sortBy $ comparing snd
+showHistogram pairs = intercalate "\n" (map (\pair -> replicate (snd pair) 'X' ++ " : " ++ fst pair) pairs)
 
 -- multiple file readin
 main = do
   args <- getArgs
 
   contents <- 
-    fmap (toLowerWords) $ 
+    fmap toLowerWords $ 
     if null args
       then getContents
-      else fmap (intercalate "") $ sequence $ map readFile args
+      else fmap (intercalate "") $ mapM readFile args
 
   let histogramData = Map.toList $ Map.fromListWith (+) (zip contents (repeat 1))
   putStrLn $ showHistogram $ sortPairs histogramData
