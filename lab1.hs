c1905c88a7edd517824a9ecd590f67501767d256
Grant Mathews <grant@causes.com>
2011-10-01 14:23:55 -0700

    Rewrite most of the code to fit new guidelines.

--- lab1.hs
+++ lab1.hs
@@ -2,15 +2,38 @@ import qualified Data.Map as Map
 import Data.Char
 import Data.List
 import Data.Ord
+import Control.Arrow
 import System.Environment
 
-toLowerWords str = map (map toLower) (words str)
-sortPairs = sortBy $ comparing snd
-showHistogram pairs = intercalate "\n" (map (\pair -> replicate (snd pair) 'X' ++ " : " ++ fst pair) pairs)
+data HistogramKey = Key (String, Int) deriving (Show)
 
--- multiple file readin
+getName (Key (name, _)) = name
+
+strToLower = map toLower
+
+stripPunctuation word 
+  | startsWithPunct word = stripPunctuation $ tail word
+  | startsWithPunct $ reverse word = stripPunctuation $ init word
+  | otherwise = word
+  where
+    punctuation = "!@#$%^&*()_+-={}|[]\\:\";'<>?,./"
+    startsWithPunct str = any id (map (== head str) punctuation) 
+
+toLowerWords str = map (stripPunctuation . strToLower) (words str)
+
+padWord word padLength = word ++ replicate (padLength - length word) ' '
+
+showKey longest (Key (name, count)) = padWord name longest ++ " : " ++ replicate count 'X'
+
+showHistogram :: [HistogramKey] -> IO ()
+showHistogram pairs = do
+  let longest = length $ maximumBy (comparing length) (map getName pairs)
+  putStrLn $ intercalate "\n" $ map (showKey longest) pairs
+
+-- 80 char max
 main = do
-  args <- getArgs
+  --args <- getArgs
+  let args = ["test"]
 
   contents <- 
     fmap toLowerWords $ 
@@ -18,5 +41,5 @@ main = do
       then getContents
       else fmap (intercalate "") $ mapM readFile args
 
-  let histogramData = Map.toList $ Map.fromListWith (+) (zip contents (repeat 1))
-  putStrLn $ showHistogram $ sortPairs histogramData
+  let countPairs = map Key $ sortBy (comparing snd) $ map (head &&& length) $ group $ sort contents
+  showHistogram countPairs
